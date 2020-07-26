2387bf59a6367e21e233e867f49afc3e8ce4599f
Felipe Nunes <felipenno14@gmail.com>
2020-07-26 02:20:30 -0300

    Telas criadas

---
+++ UI/Principal.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Lógica interna para Principal.xaml
+    /// </summary>
+    public partial class Principal : Window
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
